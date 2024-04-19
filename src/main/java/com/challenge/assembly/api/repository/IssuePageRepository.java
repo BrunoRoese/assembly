2303b09e8864307c1bcdc302eb1a10e5abaf2fc6
Bruno Roese <brunoroese@Brunos-MacBook-Air.local>
2024-04-18 23:05:41 -0300

    feat:implement new interface for issue pagination

--- src/main/java/com/challenge/assembly/api/repository/IssuePageRepository.java
+++ src/main/java/com/challenge/assembly/api/repository/IssuePageRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IssueRepository extends PagingAndSortingRepository<Issue, Integer> {
+public interface IssuePageRepository extends PagingAndSortingRepository<Issue, Integer> {
 }
