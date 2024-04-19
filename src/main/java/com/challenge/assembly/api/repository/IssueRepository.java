572d4f07e67e4c8c6c208be0142c1141941d872b
Bruno Roese <brunoroese@Brunos-MacBook-Air.local>
2024-04-18 23:06:00 -0300

    feat:implement new crud repository for issue

--- src/main/java/com/challenge/assembly/api/repository/IssueRepository.java
+++ src/main/java/com/challenge/assembly/api/repository/IssueRepository.java
@@ -1,2 +1,9 @@
-package com.challenge.assembly.api.repository;public class IssueRepository {
+package com.challenge.assembly.api.repository;
+
+import com.challenge.assembly.api.domain.Issue;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IssueRepository extends CrudRepository<Issue, Integer> {
 }
