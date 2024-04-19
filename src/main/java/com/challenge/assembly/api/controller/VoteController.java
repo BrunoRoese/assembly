15e3725f800f7a5de83968d336f6296007f83c09
Bruno Roese <brunoroese@Brunos-MacBook-Air.local>
2024-04-19 20:39:16 -0300

    feat:implement new vote controller

--- src/main/java/com/challenge/assembly/api/controller/VoteController.java
+++ src/main/java/com/challenge/assembly/api/controller/VoteController.java
@@ -1,2 +1,33 @@
-package com.challenge.assembly.api.controller;public class VoteController {
+package com.challenge.assembly.api.controller;
+
+import com.challenge.assembly.api.domain.Vote;
+import com.challenge.assembly.api.service.VoteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+import static com.challenge.assembly.api.utils.UuidUtils.convertUuid;
+
+@RestController
+@RequestMapping("api/v1/votes")
+@RequiredArgsConstructor
+public class VoteController {
+
+    private final VoteService voteService;
+
+    @GetMapping
+    public Page<Vote> getVotesBySession(
+            @RequestParam(name = "votingSessionId") String votingSessionId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        var pageRequest = PageRequest.of(page, size);
+
+        return voteService.getVotesForSession(convertUuid(votingSessionId), pageRequest);
+    }
 }
