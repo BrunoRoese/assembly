161b3a66179080ac33705600ce22c7e34585bbd1
Bruno Roese <brunoroese@Brunos-MacBook-Air.local>
2024-04-19 07:45:58 -0300

    feat:implement voting session service and unit test

--- src/main/java/com/challenge/assembly/api/service/VotingSessionService.java
+++ src/main/java/com/challenge/assembly/api/service/VotingSessionService.java
@@ -1,2 +1,21 @@
-package com.challenge.assembly.api.service;public class VotinSessionService {
+package com.challenge.assembly.api.service;
+
+import com.challenge.assembly.api.domain.VotingSession;
+import com.challenge.assembly.api.repository.VotingSessionPageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class VotingSessionService {
+
+    private final VotingSessionPageRepository votingSessionPageRepository;
+
+    public Page<VotingSession> pageVotingSessions(int page, int size) {
+        var pageRequest = PageRequest.of(page, size);
+
+        return votingSessionPageRepository.findAll(pageRequest);
+    }
 }
---
+++ src/test/java/com/challenge/assembly/service/VotingSessionServiceTest.java
@@ -0,0 +1,43 @@
+package com.challenge.assembly.service;
+
+import com.challenge.assembly.api.domain.VotingSession;
+import com.challenge.assembly.api.repository.VotingSessionPageRepository;
+import com.challenge.assembly.api.service.VotingSessionService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class VotingSessionServiceTest {
+
+    @Mock
+    private VotingSessionPageRepository votingSessionPageRepository;
+
+    @InjectMocks
+    private VotingSessionService votingSessionService;
+
+    @Test
+    void shouldPageVotingSessions() {
+        var page = 0;
+        var size = 10;
+        var pageRequest = PageRequest.of(page, size);
+        var votingSessionPage = new PageImpl<>(List.of(new VotingSession()));
+
+        given(votingSessionPageRepository.findAll(pageRequest)).willReturn(votingSessionPage);
+
+        var response = votingSessionService.pageVotingSessions(page, size);
+
+        verify(votingSessionPageRepository).findAll(pageRequest);
+        assertThat(response).isEqualTo(votingSessionPage);
+    }
+}
