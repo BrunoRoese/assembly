bd06de91ae0fb4c6db41e7bbc0225286d96f8fce
Bruno Roese <brunoroese@Brunos-MacBook-Air.local>
2024-04-18 23:04:00 -0300

    feat:implement controller for issue
    with it, implemented the get for pagination and post method

--- src/main/java/com/challenge/assembly/api/controller/IssueController.java
+++ src/main/java/com/challenge/assembly/api/controller/IssueController.java
@@ -1,2 +1,37 @@
-package com.challenge.assembly.api.controller;public class IssueController {
+package com.challenge.assembly.api.controller;
+
+import com.challenge.assembly.api.adapter.IssueAdapter;
+import com.challenge.assembly.api.domain.Issue;
+import com.challenge.assembly.api.dto.IssueRequest;
+import com.challenge.assembly.api.service.IssueService;
+import com.challenge.assembly.api.validation.IssueRequestValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/issues")
+@RequiredArgsConstructor
+public class IssueController {
+
+    private final IssueService issueService;
+    private final IssueAdapter issueAdapter;
+    private final IssueRequestValidator issueRequestValidator;
+
+    @GetMapping
+    public Page<Issue> getIssues(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        return issueService.getIssues(page, size);
+    }
+
+    @PostMapping
+    public Issue createIssue(@RequestBody IssueRequest issueRequest) {
+        issueRequestValidator.validateIssueRequest(issueRequest);
+
+        var issueDomain = issueAdapter.toDomain(issueRequest);
+
+        return issueService.createIssue(issueDomain);
+    }
 }
