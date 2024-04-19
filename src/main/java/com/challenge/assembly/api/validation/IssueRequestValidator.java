2c5908818b9c6b1e5e1d88f586e3718b56bcea1d
Bruno Roese <brunoroese@Brunos-MacBook-Air.local>
2024-04-18 22:42:14 -0300

    feat:implement issue request validator
    Write unit tests for issue request validator

--- src/main/java/com/challenge/assembly/api/validation/IssueRequestValidator.java
+++ src/main/java/com/challenge/assembly/api/validation/IssueRequestValidator.java
@@ -1,2 +1,15 @@
-package com.challenge.assembly.api.validation;public class IssueRequestValidator {
+package com.challenge.assembly.api.validation;
+
+import com.challenge.assembly.api.dto.IssueRequest;
+import com.challenge.assembly.api.exception.BadRequestException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IssueRequestValidator {
+
+    public void validateIssueRequest(IssueRequest issueRequest) {
+        if (issueRequest.getTitle().isBlank()) {
+            throw new BadRequestException("Title is required");
+        }
+    }
 }
---
+++ src/test/java/com/challenge/assembly/validation/IssueRequestValidatorTest.java
@@ -0,0 +1,42 @@
+package com.challenge.assembly.validation;
+
+import com.challenge.assembly.api.dto.IssueRequest;
+import com.challenge.assembly.api.exception.BadRequestException;
+import com.challenge.assembly.api.validation.IssueRequestValidator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(MockitoExtension.class)
+public class IssueRequestValidatorTest {
+
+    @InjectMocks
+    private IssueRequestValidator issueRequestValidator;
+
+    @ParameterizedTest
+    @EmptySource
+    void shouldThrowBadRequestExceptionWhenTitleIsBlank(String title) {
+        var issueRequest = mock(IssueRequest.class);
+
+        given(issueRequest.getTitle()).willReturn(title);
+
+        assertThrows(BadRequestException.class, () -> issueRequestValidator.validateIssueRequest(issueRequest));
+    }
+
+    @Test
+    void shouldNotThrowExceptionWhenTitleExists() {
+        var issueRequest = mock(IssueRequest.class);
+
+        given(issueRequest.getTitle()).willReturn("Title");
+
+        assertDoesNotThrow(() -> issueRequestValidator.validateIssueRequest(issueRequest));
+    }
+}
