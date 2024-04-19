cf4081484da46264d66da75851e29ecd656ff90b
Bruno Roese <brunoroese@Brunos-MacBook-Air.local>
2024-04-19 20:53:40 -0300

    feat:implement business rules for get votes by session id

--- src/main/java/com/challenge/assembly/api/controller/VoteController.java
+++ src/main/java/com/challenge/assembly/api/controller/VoteController.java
@@ -2,6 +2,8 @@ package com.challenge.assembly.api.controller;
 
 import com.challenge.assembly.api.domain.Vote;
 import com.challenge.assembly.api.service.VoteService;
+import com.challenge.assembly.api.service.VotingSessionService;
+import com.challenge.assembly.api.validation.VoteValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -18,14 +20,22 @@ import static com.challenge.assembly.api.mapper.UuidMapper.mapStringToUuid;
 public class VoteController {
 
     private final VoteService voteService;
+    private final VotingSessionService votingSessionService;
+    private final VoteValidator voteValidator;
 
     @GetMapping
     public Page<Vote> getVotesBySession(
             @RequestParam(name = "votingSessionId") String votingSessionId,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
+        voteValidator.validateVotingSessionId(votingSessionId);
+
+        var votingSessionUuid = mapStringToUuid(votingSessionId);
+
+        var votingSession = votingSessionService.getVotingSessionById(votingSessionUuid);
+
         var pageRequest = PageRequest.of(page, size);
 
-        return voteService.getVotesForSession(mapStringToUuid(votingSessionId), pageRequest);
+        return voteService.getVotesForSession(votingSession, pageRequest);
     }
 }
