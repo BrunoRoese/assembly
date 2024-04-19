fe27417b2d3ce32e5e96caf41bf71ebde40791b8
Bruno Roese <brunoroese@Brunos-MacBook-Air.local>
2024-04-19 20:39:47 -0300

    feat:implement new query to get votes by voting session id
    Implement new vote paging and sorting repository

--- src/main/java/com/challenge/assembly/api/repository/VotePageRepository.java
+++ src/main/java/com/challenge/assembly/api/repository/VotePageRepository.java
@@ -1,2 +1,17 @@
-package com.challenge.assembly.api.repository;public interface VotePageRepository {
+package com.challenge.assembly.api.repository;
+
+import com.challenge.assembly.api.domain.Vote;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface VotePageRepository extends PagingAndSortingRepository<Vote, UUID> {
+
+    @Query
+    public Page<Vote> getVoteByVotingSessionId(UUID votingSessionId, PageRequest page);
 }
