a251a75ee72a3e46b9510050af79c928681c5b5c
Bruno Roese <brunoroese@Brunos-MacBook-Air.local>
2024-04-19 08:58:38 -0300

    feat:implement new service to handle expiration time behavior for voting session

--- src/main/java/com/challenge/assembly/api/service/VotingSessionExpirationTimeService.java
+++ src/main/java/com/challenge/assembly/api/service/VotingSessionExpirationTimeService.java
@@ -1,2 +1,18 @@
-package com.challenge.assembly.api.service;public class VotingSessionExpirationTimeService {
+package com.challenge.assembly.api.service;
+
+import org.apache.commons.lang3.time.DateUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class VotingSessionExpirationTimeService {
+
+    public Date calculateExpirationTime(Date expirationTime) {
+        if (expirationTime == null) {
+            return DateUtils.addMinutes(new Date(), 1);
+        }
+
+        return expirationTime;
+    }
 }
---
+++ src/test/java/com/challenge/assembly/service/VotingSessionExpirationTimeServiceTest.java
@@ -0,0 +1,35 @@
+package com.challenge.assembly.service;
+
+import com.challenge.assembly.api.service.VotingSessionExpirationTimeService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Date;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(MockitoExtension.class)
+public class VotingSessionExpirationTimeServiceTest {
+
+    @InjectMocks
+    private VotingSessionExpirationTimeService votingSessionExpirationTimeService;
+
+    @Test
+    void shouldCalculateExpirationTime() {
+        var response = votingSessionExpirationTimeService.calculateExpirationTime(null);
+
+        assertNotNull(response);
+    }
+
+    @Test
+    void shouldReturnExpirationTime() {
+        var expirationTime = new Date();
+
+        var response = votingSessionExpirationTimeService.calculateExpirationTime(expirationTime);
+
+        assertThat(response).isEqualTo(expirationTime);
+    }
+}
