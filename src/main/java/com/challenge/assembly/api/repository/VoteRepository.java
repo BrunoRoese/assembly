6533770f5d9fd89d9d18fbf7cfbad34397ccc6c4
Bruno Roese <brunoroese@Brunos-MacBook-Air.local>
2024-04-19 20:40:41 -0300

    feat:implement new vote service
    Implement new method to get votes by voting session uuid

--- src/main/java/com/challenge/assembly/api/repository/VoteRepository.java
+++ src/main/java/com/challenge/assembly/api/repository/VoteRepository.java
@@ -1,2 +1,11 @@
-package com.challenge.assembly.api.repository;public interface VoteRepository {
+package com.challenge.assembly.api.repository;
+
+import com.challenge.assembly.api.domain.Vote;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface VoteRepository extends CrudRepository<Vote, UUID> {
 }
--- src/main/java/com/challenge/assembly/api/service/VoteService.java
+++ src/main/java/com/challenge/assembly/api/service/VoteService.java
@@ -1,2 +1,21 @@
-package com.challenge.assembly.api.service;public class VoteService {
+package com.challenge.assembly.api.service;
+
+import com.challenge.assembly.api.domain.Vote;
+import com.challenge.assembly.api.repository.VotePageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class VoteService {
+
+    private final VotePageRepository votePageRepository;
+
+    public Page<Vote> getVotesForSession(UUID votingSessionId, PageRequest pageRequest) {
+        return votePageRepository.getVoteByVotingSessionId(votingSessionId, pageRequest);
+    }
 }
