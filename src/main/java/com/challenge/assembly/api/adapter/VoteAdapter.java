a70937f6b8d1f302f7417b6e428735e55ddfcc05
Bruno Roese <brunoroese@Brunos-MacBook-Air.local>
2024-04-19 22:00:40 -0300

    feat:implement vote adapter

--- src/main/java/com/challenge/assembly/api/adapter/VoteAdapter.java
+++ src/main/java/com/challenge/assembly/api/adapter/VoteAdapter.java
@@ -1,2 +1,19 @@
-package com.challenge.assembly.api.adapter;public class VoteAdapter {
+package com.challenge.assembly.api.adapter;
+
+import com.challenge.assembly.api.domain.Vote;
+import com.challenge.assembly.api.dto.VoteResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VoteAdapter {
+
+    public VoteResponse toResponse(Vote vote) {
+        return new VoteResponse(
+                vote.getId(),
+                vote.getUserId(),
+                vote.getVotingSession().getId(),
+                vote.getVotingSession().getIssue().getId(),
+                vote.getStatus()
+        );
+    }
 }
