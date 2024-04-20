55d40e973f65b6e795919cbc83f1e59f7c9587f1
Bruno Roese <brunoroese@Brunos-MacBook-Air.local>
2024-04-19 21:07:32 -0300

    feat:implement new vote validator

--- src/main/java/com/challenge/assembly/api/validation/VoteValidator.java
+++ src/main/java/com/challenge/assembly/api/validation/VoteValidator.java
@@ -1,2 +1,13 @@
-package com.challenge.assembly.api.validation;public class VoteValidator {
+package com.challenge.assembly.api.validation;
+
+import com.challenge.assembly.api.exception.BadRequestException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VoteValidator {
+    public void validateVotingSessionId(String votingSessionId) {
+        if (votingSessionId == null || votingSessionId.isEmpty()) {
+            throw new BadRequestException("Voting session ID cannot be null or empty");
+        }
+    }
 }
