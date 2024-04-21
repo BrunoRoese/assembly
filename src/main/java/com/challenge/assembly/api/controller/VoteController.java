d9d4e6049e3ae88862059a78a58444314dd900be
Bruno Roese <brunoroese@Brunos-MacBook-Air.local>
2024-04-21 16:14:53 -0300

    refactor:use try catch block to handle exception properly in vote controller get

--- src/main/java/com/challenge/assembly/api/controller/VoteController.java
+++ src/main/java/com/challenge/assembly/api/controller/VoteController.java
@@ -1,7 +1,11 @@
 package com.challenge.assembly.api.controller;
 
 import com.challenge.assembly.api.adapter.VoteAdapter;
+import com.challenge.assembly.api.dto.VoteRequest;
 import com.challenge.assembly.api.dto.VoteResponse;
+import com.challenge.assembly.api.exception.BadRequestException;
+import com.challenge.assembly.api.exception.ConflictException;
+import com.challenge.assembly.api.exception.NotFoundException;
 import com.challenge.assembly.api.service.VoteService;
 import com.challenge.assembly.api.service.VotingSessionService;
 import com.challenge.assembly.api.validation.VoteValidator;
@@ -29,12 +33,35 @@ public class VoteController {
             @RequestParam(defaultValue = "10") int size) {
         voteValidator.validateVotingSessionId(votingSessionId);
 
-        var votingSessionUuid = mapStringToUuid(votingSessionId);
+        try {
+            var votingSessionUuid = mapStringToUuid(votingSessionId);
 
-        var votingSession = votingSessionService.getVotingSessionById(votingSessionUuid);
+            var votingSession = votingSessionService.getVotingSessionById(votingSessionUuid);
 
-        var pageRequest = PageRequest.of(page, size);
+            var pageRequest = PageRequest.of(page, size);
 
-        return voteService.getVotesForSession(votingSession, pageRequest).map(voteAdapter::toResponse);
+            return voteService.getVotesForSession(votingSession, pageRequest).map(voteAdapter::toResponse);
+        } catch (Exception e) {
+            throw new BadRequestException("Invalid voting session UUID");
+        }
     }
+
+//    @PostMapping
+//    public VoteResponse vote(@RequestBody VoteRequest voteRequest) {
+//        voteValidator.validateVoteRequest(voteRequest);
+//
+//        try {
+//            var voteResponseUuid = mapStringToUuid(voteRequest.votingSessionId());
+//
+//            votingSessionService.getVotingSessionById()
+//        } catch (ConflictException e) {
+//            throw new BadRequestException("Invalid UUID");
+//        }
+//
+//        var votingSession = votingSessionService.getVotingSessionById(mapStringToUuid(voteRequest.votingSessionId()));
+//
+//        var vote = voteAdapter.toEntity(voteResponse);
+//
+//        return voteAdapter.toResponse(voteService.vote(vote));
+//    }
 }
