cad2c45c39950784c5aaf4e43f8b03816cbc3999
Bruno Roese <brunoroese@Brunos-MacBook-Air.local>
2024-04-21 19:27:12 -0300

    feat:implement method in vote adapater to transform vote into domain

--- src/main/java/com/challenge/assembly/api/adapter/VoteAdapter.java
+++ src/main/java/com/challenge/assembly/api/adapter/VoteAdapter.java
@@ -1,8 +1,15 @@
 package com.challenge.assembly.api.adapter;
 
 import com.challenge.assembly.api.domain.Vote;
+import com.challenge.assembly.api.domain.VoteStatus;
+import com.challenge.assembly.api.domain.VotingSession;
+import com.challenge.assembly.api.dto.VoteRequest;
 import com.challenge.assembly.api.dto.VoteResponse;
+import com.challenge.assembly.api.exception.BadRequestException;
+import com.challenge.assembly.api.exception.ConflictException;
 import org.springframework.stereotype.Component;
+
+import static com.challenge.assembly.api.mapper.UuidMapper.mapStringToUuid;
 
 @Component
 public class VoteAdapter {
@@ -15,5 +22,15 @@ public class VoteAdapter {
                 vote.getVotingSession().getIssue().getId(),
                 vote.getStatus()
         );
+    }
+
+    public Vote toDomain(VoteRequest voteRequest, VotingSession votingSession) {
+        try {
+            var userId = mapStringToUuid(voteRequest.userId());
+
+            return new Vote(userId, voteRequest.voteStatus(), votingSession);
+        } catch (ConflictException e) {
+            throw new BadRequestException("Invalid user UUID");
+        }
     }
 }
