4e6a3a1785378065108135f92a9c792ec42de280
Bruno Roese <brunoroese@Brunos-MacBook-Air.local>
2024-04-21 19:29:05 -0300

    test:implement unit test for vote adapter to domain method

--- src/test/java/com/challenge/assembly/adapter/VoteAdapterTest.java
+++ src/test/java/com/challenge/assembly/adapter/VoteAdapterTest.java
@@ -5,6 +5,10 @@ import com.challenge.assembly.api.domain.Issue;
 import com.challenge.assembly.api.domain.Vote;
 import com.challenge.assembly.api.domain.VoteStatus;
 import com.challenge.assembly.api.domain.VotingSession;
+import com.challenge.assembly.api.dto.VoteRequest;
+import com.challenge.assembly.api.exception.BadRequestException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,6 +17,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
@@ -47,5 +52,38 @@ class VoteAdapterTest {
         assertThat(response.votingSessionId()).isEqualTo(votingSessionId);
         assertThat(response.issueId()).isEqualTo(issueId);
         assertThat(response.voteStatus()).isEqualTo(VoteStatus.YES);
+    }
+
+    @Nested
+    class ToDomain {
+        private VotingSession votingSession;
+        private VoteRequest voteRequest;
+
+        @BeforeEach
+        void setup() {
+            voteRequest = mock(VoteRequest.class);
+            votingSession = mock(VotingSession.class);
+        }
+
+        @Test
+        void shouldThrowBadRequestOnInvalidUserId() {
+            given(voteRequest.userId()).willReturn("invalid-uuid");
+
+            assertThrows(BadRequestException.class, () -> voteAdapter.toDomain(voteRequest, votingSession));
+        }
+
+        @Test
+        void shouldConvertToDomain() {
+            var validUserId = UUID.randomUUID().toString();
+
+            given(voteRequest.userId()).willReturn(validUserId);
+            given(voteRequest.voteStatus()).willReturn(VoteStatus.YES);
+
+            var vote = voteAdapter.toDomain(voteRequest, votingSession);
+
+            assertThat(vote.getUserId().toString()).isEqualTo(validUserId);
+            assertThat(vote.getStatus()).isEqualTo(VoteStatus.YES);
+            assertThat(vote.getVotingSession()).isEqualTo(votingSession);
+        }
     }
 }
