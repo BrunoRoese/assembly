68afd2e54ed411dda58baf39153859fc6ec86e5d
Bruno Roese <brunoroese@Brunos-MacBook-Air.local>
2024-04-21 18:43:20 -0300

    feat:implement new exception for not found excetion

--- src/main/java/com/challenge/assembly/api/exception/NotFoundException.java
+++ src/main/java/com/challenge/assembly/api/exception/NotFoundException.java
@@ -1,2 +1,7 @@
-package com.challenge.assembly.api.exception;public class NotFoundException {
+package com.challenge.assembly.api.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
 }
