972cdcc5af3846be9161ddbb0e11cd9e9e324bc0
Bruno Roese <brunoroese@192.168.0.3>
2024-04-21 22:09:10 -0300

    feat:write query to get vote by voting session and userId

--- src/main/java/com/challenge/assembly/api/repository/VoteRepository.java
+++ src/main/java/com/challenge/assembly/api/repository/VoteRepository.java
@@ -1,11 +1,17 @@
 package com.challenge.assembly.api.repository;
 
 import com.challenge.assembly.api.domain.Vote;
+import com.challenge.assembly.api.domain.VotingSession;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface VoteRepository extends CrudRepository<Vote, UUID> {
+
+    @Query
+    public Optional<Vote> findByVotingSessionAndUserId(VotingSession votingSession, UUID userId);
 }
