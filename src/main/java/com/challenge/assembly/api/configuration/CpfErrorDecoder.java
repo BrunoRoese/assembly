8d6fe776c066cd5958ef31f03828c74b11b0ec38
Bruno Roese <brunoroese@Brunos-MacBook-Air.local>
2024-04-22 08:58:42 -0300

    feat:implement cpf error decoder for open feign decode

--- src/main/java/com/challenge/assembly/api/configuration/CpfErrorDecoder.java
+++ src/main/java/com/challenge/assembly/api/configuration/CpfErrorDecoder.java
@@ -1,2 +1,16 @@
-package com.challenge.assembly.api.configuration;public class ErrorDecoder {
+package com.challenge.assembly.api.configuration;
+
+import com.challenge.assembly.api.exception.BadRequestException;
+import feign.Response;
+
+public class CpfErrorDecoder implements feign.codec.ErrorDecoder {
+
+        @Override
+        public Exception decode(String methodKey, Response response) {
+            if (response.status() == 404) {
+                return new BadRequestException("Invalid CPF");
+            }
+
+            return new RuntimeException("Internal Server Error");
+        }
 }
--- src/main/java/com/challenge/assembly/api/configuration/CpfValidatorConfiguration.java
+++ src/main/java/com/challenge/assembly/api/configuration/CpfValidatorConfiguration.java
@@ -1,2 +1,14 @@
-package com.challenge.assembly.api.configuration;public class CpfValidatorConfiguration {
+package com.challenge.assembly.api.configuration;
+
+import feign.codec.ErrorDecoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CpfValidatorConfiguration {
+
+    @Bean
+    public ErrorDecoder errorDecoder() {
+        return new CpfErrorDecoder();
+    }
 }
