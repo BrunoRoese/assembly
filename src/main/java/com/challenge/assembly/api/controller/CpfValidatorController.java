0495fa260e2c40e39dda3a0835b49b388e5849c5
Bruno Roese <brunoroese@Brunos-MacBook-Air.local>
2024-04-22 08:59:18 -0300

    feat:implement cpf validator controller

--- src/main/java/com/challenge/assembly/api/controller/CpfValidatorController.java
+++ src/main/java/com/challenge/assembly/api/controller/CpfValidatorController.java
@@ -1,2 +1,27 @@
-package com.challenge.assembly.api.controller;public class CpfValidatorController {
+package com.challenge.assembly.api.controller;
+
+import com.challenge.assembly.api.client.CpfValidatorClient;
+import com.challenge.assembly.api.exception.BadRequestException;
+import com.challenge.assembly.api.exception.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/cpf-validator")
+@RequiredArgsConstructor
+public class CpfValidatorController {
+
+    private final CpfValidatorClient cpfValidatorClient;
+
+    @GetMapping("/{cpf}")
+    public String validateCpf(@PathVariable("cpf") String cpf) {
+        try {
+            return cpfValidatorClient.validateCpf(cpf);
+        } catch (NotFoundException e) {
+            throw new BadRequestException("Invalid CPF");
+        }
+    }
 }
